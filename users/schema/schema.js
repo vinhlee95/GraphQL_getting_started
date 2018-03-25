233ad8b7490d793d5d541409c82d3c7e4ef881c8
Vinhlee95 <lethanhvinh95@gmail.com>
2018-03-25 08:09:24 +0300

    ADD rootquery & export schema

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,10 +1,18 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
    GraphQLObjectType,
    GraphQLString,
-   GraphQLInt
+   GraphQLInt,
+   GraphQLSchema
 } = graphql;
+
+const users = [
+   { id: 23, firstName: 'Bill', age: 47},
+   { id: 34, firstName: 'Alex', age: 18 },
+   { id: 24, firstName: 'Amanda', age: 33},
+];
 
 const UserType = new GraphQLObjectType({
    name: 'User',
@@ -14,4 +22,22 @@ const UserType = new GraphQLObjectType({
       firstName: { type: GraphQLString },
       age: { type: GraphQLInt },
    }
-})
+});
+
+const RootQuery = new GraphQLObjectType({
+   name: 'RootQueryType',
+   fields: {
+      user: {
+         type: UserType,
+         args: { id: { type: GraphQLString }},
+         // return an user with a given id
+         resolve() {
+            return _.find(users, {id: args.id});
+         }
+      }
+   }
+});
+
+module.exports = new GraphQLSchema({
+   query: RootQuery
+});
